627417eed6dc5a437b94c1e5e9ae4f00c8f200ee
Brandon Smith <brandon93s@gmail.com>
2017-11-13 20:44:27 -0500

    Add multi-language support

--- test.js
+++ test.js
@@ -7,6 +7,11 @@ import tempy from 'tempy';
 const bin = path.join(__dirname, 'cli.js');
 const fixture = fs.readFileSync(path.join(__dirname, 'fixtures/code-of-conduct.md'), 'utf8');
 
+test.beforeEach(async () => {
+	const cwd = tempy.directory();
+	await execa(bin, ['--language=en'], {cwd});
+});
+
 test('generate', async t => {
 	const cwd = tempy.directory();
 	await execa(bin, ['--email=foo@bar.com'], {cwd});
@@ -23,4 +28,36 @@ test('update', async t => {
 	const src = fs.readFileSync(filepath, 'utf8');
 	t.true(src.includes('In the interest of fostering'));
 	t.true(src.includes('fixture@bar.com'));
+});
+
+test('default language', async t => {
+	const cwd = tempy.directory();
+	await execa(bin, {cwd});
+	const src = fs.readFileSync(path.join(cwd, 'code-of-conduct.md'), 'utf8');
+	t.true(src.includes('In the interest of fostering'));
+});
+
+test('set language', async t => {
+	const cwd = tempy.directory();
+	await execa(bin, ['--language=es'], {cwd});
+	const src = fs.readFileSync(path.join(cwd, 'code-of-conduct.md'), 'utf8');
+	t.true(src.includes('En el interés de fomentar'));
+});
+
+test('unsupported language', async t => {
+	const cwd = tempy.directory();
+	try {
+		await execa(bin, ['--language=unicorn'], {cwd});
+	} catch (err) {
+		t.true(err.stderr.includes(`Unsupported language 'unicorn'`));
+	}
+});
+
+test('update language', async t => {
+	const cwd = tempy.directory();
+	const filepath = path.join(cwd, 'CODE_OF_CONDUCT.markdown');
+	fs.writeFileSync(filepath, fixture);
+	await execa(bin, ['--language=es'], {cwd});
+	const src = fs.readFileSync(filepath, 'utf8');
+	t.true(src.includes('En el interés de fomentar'));
 });
